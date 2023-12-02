c711c0b3257b0b2c30f5b3f7b82723f6f45f8d5b
Emir-Asanov <asanovemirsuyun@gmail.com>
2023-12-02 14:56:36 +0200

    add firebase

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -12,13 +12,13 @@ import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:url_launcher_android/url_launcher_android.dart';
 import 'package:google_sign_in_ios/google_sign_in_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
-import 'package:shared_preferences_ios/shared_preferences_ios.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:url_launcher_ios/url_launcher_ios.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
 import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:url_launcher_linux/url_launcher_linux.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
-import 'package:shared_preferences_macos/shared_preferences_macos.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:url_launcher_macos/url_launcher_macos.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
 import 'package:shared_preferences_windows/shared_preferences_windows.dart';
@@ -86,10 +86,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SharedPreferencesIOS.registerWith();
+        SharedPreferencesFoundation.registerWith();
       } catch (err) {
         print(
-          '`shared_preferences_ios` threw an error: $err. '
+          '`shared_preferences_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -142,10 +142,10 @@ class _PluginRegistrant {
       }
 
       try {
-        SharedPreferencesMacOS.registerWith();
+        SharedPreferencesFoundation.registerWith();
       } catch (err) {
         print(
-          '`shared_preferences_macos` threw an error: $err. '
+          '`shared_preferences_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
